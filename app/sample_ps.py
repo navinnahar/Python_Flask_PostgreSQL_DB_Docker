9f644b29e61c1f84b38b4fe92defae30a9e66db8
root <root@myubuntuvm.yyftpvp1p1curdsj0jhcamlerg.gx.internal.cloudapp.net>
2017-06-14 09:38:35 +0000

    test

---
+++ app/sample_ps.py
@@ -0,0 +1,6 @@
+virtualenv venv
+source venv/bin/activate
+pip install -r requirements.txt
+cd app
+FLASK_APP=app.py DBHOST="localhost" DBUSER="manager" DBNAME="eventregistration" DBPASS="supersecretpass" flask db upgrade
+FLASK_APP=app.py DBHOST="localhost" DBUSER="manager" DBNAME="eventregistration" DBPASS="supersecretpass" flask db run
